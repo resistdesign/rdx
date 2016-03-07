2d7056433411347e7ee87f2edcee5739b3c27151
Ryan Graff <ryan@StellarOmega.local>
2016-03-06 22:53:15 -0500

    feat(rdx.js) Make asynchronous.

--- src/rdx.js
+++ src/rdx.js
@@ -6,10 +6,24 @@ const COMMAND_ROOT = Path.join(__dirname, 'Commands');
 const ARGS = Minimist(process.argv.slice(2));
 const COMMANDS = (ARGS || {})._;
 
-if (COMMANDS instanceof Array && COMMANDS.length) {
-  const cmd = COMMANDS[0];
+async function RDX() {
+  if (COMMANDS instanceof Array && COMMANDS.length) {
+    const cmd = COMMANDS[0];
 
-  Command.exec(cmd, ARGS, COMMAND_ROOT);
-} else {
-  Command.exec('default', ARGS, COMMAND_ROOT);
+    await Command.exec(cmd, ARGS, COMMAND_ROOT);
+  } else {
+    await Command.exec('default', ARGS, COMMAND_ROOT);
+  }
 }
+
+const startTime = new Date().getTime();
+
+RDX()
+  .then(() => {
+    const endTime = new Date().getTime();
+    const secs = (endTime - startTime) / 1000;
+
+    console.log('FINISHED IN:', `${secs} seconds.`.yellow);
+  }, error => {
+    console.error(error);
+  });
