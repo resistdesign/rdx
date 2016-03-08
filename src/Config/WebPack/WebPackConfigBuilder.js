4beb7d6b936c511de4595109f4967eaaed4d201a
Ryan Graff <ryan@StellarOmega.local>
2016-03-07 22:44:48 -0500

    feat() Add WebPackConfigBuilder.

---
+++ src/Config/WebPack/WebPackConfigBuilder.js
@@ -0,0 +1,114 @@
+import WebPack from 'webpack';
+import ExtractTextPlugin from  'extract-text-webpack-plugin';
+
+export default class WebPackConfigBuilder {
+  static CONFIG_TYPES = {
+    SERVE: 'SERVE',
+    COMPILE: 'COMPILE'
+  };
+
+  static PLUGINS = {
+    SERVE: [
+      new WebPack.HotModuleReplacementPlugin(),
+      new WebPack.NoErrorsPlugin()
+    ],
+    COMPILE: [
+      new WebPack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new WebPack.optimize.UglifyJsPlugin(),
+      new WebPack.optimize.OccurenceOrderPlugin(),
+      new ExtractTextPlugin('styles.css')
+    ]
+  };
+
+  static LOADERS = {
+    jsHot: {// Serve
+      TYPE: WebPackConfigBuilder.CONFIG_TYPES.SERVE,
+      test: /\.(js|jsx)$/,
+      loader: require.resolve('react-hot-loader') + '!' + require.resolve('babel-loader') + '?stage=0',
+      include: packageRoot
+    },
+    js: {// Compile
+      TYPE: WebPackConfigBuilder.CONFIG_TYPES.COMPILE,
+      test: /\.(js|jsx)$/,
+      loader: require.resolve('babel-loader') + '?stage=0',
+      include: packageRoot
+    },
+
+    json: {
+      test: /\.json$/,
+      loader: require.resolve('json-loader')
+    },
+
+    style: {// Serve
+      TYPE: WebPackConfigBuilder.CONFIG_TYPES.SERVE,
+      test: /\.css$/,
+      loader: require.resolve('style-loader')
+    },
+    extractCss: {// Compile
+      TYPE: WebPackConfigBuilder.CONFIG_TYPES.COMPILE,
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract(require.resolve('css-loader') + '?safe')
+    },
+    css: {// Serve
+      TYPE: WebPackConfigBuilder.CONFIG_TYPES.SERVE,
+      test: /\.css$/,
+      loader: require.resolve('css-loader') + '?safe'
+    },
+    postCss: {
+      test: /\.css$/,
+      loader: require.resolve('postcss-loader')
+    },
+
+    image: {
+      test: /\.(png|jpg|svg)$/,
+      loader: require.resolve('url-loader') + '?limit=8192'
+    },
+
+    woff: {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: require.resolve('url-loader') + '?limit=10000&mimetype=application/font-woff'
+    },
+    ttf: {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: require.resolve('url-loader') + '?limit=10000&mimetype=application/octet-stream'
+    },
+    eot: {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: require.resolve('file-loader')
+    },
+    svg: {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: require.resolve('url-loader') + '?limit=10000&mimetype=image/svg+xml'
+    },
+    otf: {
+      test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: require.resolve('url-loader') + '?limit=10000&mimetype=font/opentype'
+    }
+  };
+
+  static getBaseConfig(type = WebPackConfigBuilder.CONFIG_TYPES.COMPILE) {
+    const newConfig = {
+      plugins: type === WebPackConfigBuilder.CONFIG_TYPES.SERVE ?
+        WebPackConfigBuilder.PLUGINS.SERVE : WebPackConfigBuilder.CONFIG_TYPES.COMPILE,
+      module: {
+        loaders: []
+      }
+    };
+
+    for (let k in WebPackConfigBuilder.LOADERS) {
+      const loader = WebPackConfigBuilder.LOADERS[k];
+      const loaderType = loader.TYPE;
+      const newLoader = {...loader};
+
+      delete loader.TYPE;
+
+      if (typeof loaderType === 'undefined' || loaderType === type) {
+        newConfig.module.loaders.push(newLoader);
+      }
+    }
+
+    return newConfig;
+  }
+}
