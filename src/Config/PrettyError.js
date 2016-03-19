80f928366ae1349530fe8a6baec092085468cf4d
Ryan Graff <ryan@StellarOmega.local>
2016-03-19 03:01:26 -0400

    feat() Improve error colors.

--- src/Config/PrettyError.js
+++ src/Config/PrettyError.js
@@ -14,14 +14,14 @@ export default {
   // our error message
   'pretty-error > header > message': {
     // let's change its color:
-    color: 'red',
+    color: 'bright-white',
 
     // we can use black, red, green, yellow, blue, magenta, cyan, white,
     // grey, bright-red, bright-green, bright-yellow, bright-blue,
     // bright-magenta, bright-cyan, and bright-white
 
     // we can also change the background color:
-    background: 'grey',
+    background: 'red',
 
     // it understands paddings too!
     padding: '0 1' // top/bottom left/right
