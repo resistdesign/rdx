df8d572a8c1bf8075ec4bb66d103f6bf17b595df
Ryan Graff <ryan@StellarOmega.local>
2016-12-29 02:19:14 -0500

    fix(BabelExcludes) Improve regex entries.

--- src/Config/WebPack/Constants/BabelExcludes.js
+++ src/Config/WebPack/Constants/BabelExcludes.js
@@ -1,5 +1,5 @@
 export default [
-  /node_modules\/babel-polyfill/m,
-  /node_modules\/babel-runtime/m,
-  /node_modules\/core-js/m
+  /node_modules\/babel-/m,
+  /node_modules\/core-js\//m,
+  /node_modules\/regenerator-runtime\//m
 ];
