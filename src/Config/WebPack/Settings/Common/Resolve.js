3ccc4f81bff8c18497d16ae0fc84defc54eeaaeb
Ryan Graff <ryan@StellarOmega.local>
2017-01-26 01:50:17 -0500

    fix: Upgrade to use React and React DOM ^15.4.0.

--- src/Config/WebPack/Settings/Common/Resolve.js
+++ src/Config/WebPack/Settings/Common/Resolve.js
@@ -4,7 +4,8 @@ export default function (contextPath, outputPath) {
   return {
     resolve: {
       alias: {
-        'react': Path.join(process.cwd(), 'node_modules', 'react')
+        'react': Path.join(process.cwd(), 'node_modules', 'react'),
+        'react-dom': Path.join(process.cwd(), 'node_modules', 'react-dom')
       },
       fallback: [
         Path.resolve(__dirname, '..', '..', '..', '..', '..', 'node_modules')
