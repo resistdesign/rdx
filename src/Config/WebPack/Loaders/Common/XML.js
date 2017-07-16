23b83a031a8bd93a864b4c06145ce3929b3fd09a
Ryan Graff <ryantgraff@resistdesign.com>
2017-07-16 18:25:40 -0400

    fix: (src/Config/WebPack/Loaders/Common/XML.js) Fix a pathing issue on Windows.

--- src/Config/WebPack/Loaders/Common/XML.js
+++ src/Config/WebPack/Loaders/Common/XML.js
@@ -2,7 +2,7 @@ export default function (contextPath, outputPath) {
   const fileLoaderPath = `${require.resolve('file-loader')}?context=${contextPath}&name=[path][name].[ext]?[hash]`;
   return [
     {
-      test: /(^|\/)browserconfig\.xml$/,
+      test: /(^|\/|\\)browserconfig\.xml$/,
       loader: fileLoaderPath
     }
   ];
