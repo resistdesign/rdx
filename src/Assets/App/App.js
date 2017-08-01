192bb635a301aebf39c53c343e85e6c720205997
Teresa Lee <teresa@scientificlee.com>
2017-08-01 15:05:47 -0400

    feat: () add prop-types to dependencies

--- src/Assets/App/App.js
+++ src/Assets/App/App.js
@@ -1,8 +1,9 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class {{name}} extends Component {
   static propTypes = {};
-  
+
   constructor() {
     super();
   }
