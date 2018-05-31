19649c2f15e4d861795f82db5e2bfb6450dae6b7
Ryan Graff <ryantgraff@resistdesign.com>
2018-05-30 21:42:58 -0400

    fix: (WebPack ENV Config) Fix how process is defined.

--- src/Config/WebPack/Plugins/Common/ENV.jsx
+++ src/Config/WebPack/Plugins/Common/ENV.jsx
@@ -1,29 +1,27 @@
 import WebPack from 'webpack';
-import Flat from 'flat';
 
 const DEVELOPMENT_ENV = 'development';
 const CURRENT_ENV = process.env.NODE_ENV || DEVELOPMENT_ENV;
-const DEFINITIONS = Flat({
-  process: {
-    env: {
-      NODE_ENV: process.env && process.env.NODE_ENV,
-      DEBUG: process.env && process.env.DEBUG,
-      IS: {
-        [CURRENT_ENV]: true
-      }
+const PROCESS = {
+  env: {
+    NODE_ENV: process.env && process.env.NODE_ENV,
+    DEBUG: process.env && process.env.DEBUG,
+    IS: {
+      [CURRENT_ENV]: true
     }
   }
-});
-const DEFINITIONS_WITH_JSON_VALUES = Object
-  .keys(DEFINITIONS)
-  .reduce((acc, k) => {
-    acc[k] = JSON.stringify(DEFINITIONS[k]);
-
-    return acc;
-  }, {});
+};
+const DEFINITIONS = {
+  'process.env.NODE_ENV': JSON.stringify(PROCESS.env.NODE_ENV),
+  'process.env.DEBUG': JSON.stringify(PROCESS.env.DEBUG),
+  [`process.env.IS.${CURRENT_ENV}`]: JSON.stringify(PROCESS.env.IS[CURRENT_ENV]),
+  'process.env.IS': JSON.stringify(PROCESS.env.IS),
+  'process.env': JSON.stringify(PROCESS.env),
+  'process': JSON.stringify(PROCESS)
+};
 
 export default function () {
   return [
-    new WebPack.DefinePlugin(DEFINITIONS_WITH_JSON_VALUES)
+    new WebPack.DefinePlugin(DEFINITIONS)
   ];
 }
