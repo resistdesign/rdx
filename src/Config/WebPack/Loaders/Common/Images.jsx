808a93c0d01b593fd583d19b2a9877e63fcb2c4c
Ryan Graff <ryantgraff@resistdesign.com>
2019-06-14 00:50:16 -0400

    feat: (WebPack Config) Add better media and image support.

--- src/Config/WebPack/Loaders/Common/Images.jsx
+++ src/Config/WebPack/Loaders/Common/Images.jsx
@@ -2,7 +2,7 @@ export default function (contextPath) {
   const fileLoaderPath = `${require.resolve('file-loader')}?context=${contextPath}&name=[path][name].[ext]?[hash]`;
   return [
     {
-      test: /\.(png|jpg|jpeg|gif|ico|svg)($|\?v=\d+\.\d+\.\d+$)/,
+      test: /\.(webp|png|jpg|jpeg|gif|ico|svg)($|\?v=\d+\.\d+\.\d+$)/,
       loader: fileLoaderPath
     }
   ];
