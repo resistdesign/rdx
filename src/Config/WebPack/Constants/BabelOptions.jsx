7130b203b36f24c1fc0e10d4b5f98612022ee023
Ryan Graff <ryantgraff@resistdesign.com>
2019-12-09 13:32:03 -0500

    fix: (BabelOptions) Fix Babel plug-in order.

--- src/Config/WebPack/Constants/BabelOptions.jsx
+++ src/Config/WebPack/Constants/BabelOptions.jsx
@@ -2,11 +2,11 @@ export default {
   babelrc: false,
   plugins: [
     require.resolve('react-hot-loader/babel'),
+    require.resolve('babel-plugin-transform-flow-strip-types'),
     require.resolve('babel-plugin-transform-react-jsx'),
     require.resolve('babel-plugin-transform-class-properties'),
     require.resolve('babel-plugin-transform-object-rest-spread'),
     require.resolve('babel-plugin-transform-function-bind'),
-    require.resolve('babel-plugin-transform-flow-strip-types'),
     require.resolve('babel-plugin-add-module-exports'),
     [
       require.resolve('babel-plugin-transform-runtime'),
