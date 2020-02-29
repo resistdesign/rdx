3995b53b0398260500b3478662f6867d4b0ca8f7
Ryan Graff <ryantgraff@resistdesign.com>
2020-02-29 11:44:31 -0500

    feat: (Loaders) Add the stub for the HTML loader with TODOs.

---
+++ src/Loaders/HTML.js
@@ -0,0 +1,9 @@
+module.exports = function (content) {
+  // TODO: Get a tree from the HTML content.
+  const tree = {};
+  // TODO: Search for dependency paths.
+  // TODO: Add dependencies.
+  // TODO: Replace paths in `tree` with those appropriate for the output.
+  // TODO: Add cache busting (with a tag attribute option to disable it) to each output path.
+  // TODO: Convert the `tree` back to HTML and return it.
+};
