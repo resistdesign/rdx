423fbc5b8dee6ef43c9f2e81398b4835ed1dcccf
Ryan Graff <ryantgraff@resistdesign.com>
2020-03-04 23:01:43 -0500

    feat: (Utils) Add the basis for HTMLConfig.

---
+++ src/Utils/HTMLConfig.js
@@ -0,0 +1,58 @@
+const Path = require('path');
+const FS = require('fs');
+const Cheerio = require('cheerio');
+
+const getRelativeImportOutputPath = ({
+                                       fullContextPath = '',
+                                       fullRequesterFilePath = '',
+                                       relativeImportPath = ''
+                                     } = {}) => Path
+  .join(
+    Path.relative(
+      fullContextPath,
+      Path.dirname(fullRequesterFilePath)
+    ),
+    relativeImportPath
+  );
+
+class HTMLConfig {
+  hash;
+  fullFilePath;
+  fullContextPath;
+
+  constructor (config = {}) {
+    Object.assign(this, config);
+  }
+
+  getCurrentData = () => {
+    const self = this;
+    const content = FS.readFileSync(this.fullFilePath, { encoding: 'utf8' });
+    const parser = Cheerio.load(content);
+    const hrefNodes = parser('[href]:not(a)');
+    const srcNodes = parser('[src]');
+    const entry = {};
+    const getPathProcessor = (attrName = '') => function () {
+      const elem = parser(this);
+      const sourcePath = elem.attr(attrName);
+      const outputPath = getRelativeImportOutputPath({
+        fullContextPath: self.fullContextPath,
+        fullRequesterFilePath: self.fullFilePath,
+        relativeImportPath: sourcePath
+      });
+
+      entry[outputPath] = outputPath;
+
+      elem.attr(attrName, `${sourcePath}?${self.hash}`);
+    };
+
+    hrefNodes.each(getPathProcessor('href'));
+    srcNodes.each(getPathProcessor('src'));
+
+    return {
+      content,
+      entry
+    };
+  };
+}
+
+module.exports = HTMLConfig;
