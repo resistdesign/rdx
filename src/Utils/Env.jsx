633918f11ca11c879b30f6f52cd85fdefeb809dc
Ryan Graff <ryantgraff@resistdesign.com>
2020-03-28 13:45:38 -0400

    refactor: (Utils > Env) Convert to ES6 module syntax.

--- src/Utils/Env.jsx
+++ src/Utils/Env.jsx
@@ -2,15 +2,14 @@ const PRODUCTION_NODE_ENV = 'production';
 const DEVELOPMENT_NODE_ENV = 'development';
 const DEFAULT_NODE_ENV = PRODUCTION_NODE_ENV;
 
-/**
- * Sets up the `NODE_ENV` upon import.
- * */
 process.env = !!process.env ? process.env : {};
 process.env.NODE_ENV = !!process.env.NODE_ENV ? process.env.NODE_ENV : DEFAULT_NODE_ENV;
 
-export default {
+export const ENV_VARS = {
   DEFAULT_NODE_ENV,
   PRODUCTION_NODE_ENV,
   DEVELOPMENT_NODE_ENV,
   ENV: process.env
 };
+
+export default ENV_VARS;
