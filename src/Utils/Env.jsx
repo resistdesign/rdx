8054ff18d1de36f22e7f23474f4c17144a7fc6a3
Ryan Graff <ryantgraff@resistdesign.com>
2020-03-28 13:45:24 -0400

    refactor: (Utils > Env) Convert to ES6 module syntax.

--- src/Utils/Env.jsx
+++ src/Utils/Env.jsx
@@ -8,20 +8,9 @@ const DEFAULT_NODE_ENV = PRODUCTION_NODE_ENV;
 process.env = !!process.env ? process.env : {};
 process.env.NODE_ENV = !!process.env.NODE_ENV ? process.env.NODE_ENV : DEFAULT_NODE_ENV;
 
-/**
- * Environment related values.
- * */
-module.exports = {
-  /**
-   * The default value for `ENV.NODE_ENV`.
-   * @type {string}
-   * */
+export default {
   DEFAULT_NODE_ENV,
   PRODUCTION_NODE_ENV,
   DEVELOPMENT_NODE_ENV,
-  /**
-   * The current process environment object.
-   * @type {Object}
-   * */
   ENV: process.env
 };
