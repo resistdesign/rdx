5987c161c32a0c94c1d707991042408293ec593b
Ryan Graff <ryantgraff@resistdesign.com>
2020-04-18 13:10:49 -0400

    chore: (rdx-app) Clean-up old code.

--- src/rdx-app.jsx
+++ src/rdx-app.jsx
@@ -1,13 +1,6 @@
 #!/usr/bin/env node
 
-const Path = require('path');
-const Glob = require('glob');
 const Program = require('commander');
-const {
-  getMergedOptions,
-  getPackage
-} = require('./Utils/Package');
-const { execCommandInline } = require('./Utils/CommandLine');
 
 /**
  * The App program.
@@ -18,50 +11,17 @@ Program
   .option('-b, --base <directory>', 'The base directory for app files.', 'src')
   .parse(process.argv);
 
+// currentWorkingDirectory: string;
+// title: string;
+// description: string;
+// themeColor: string;
+// baseDirectory: string;
+// includeIcons: boolean;
+// isDefaultApp: boolean;
+// overwrite: boolean;
+
 const exec = async () => {
-  // Constants
-  const ASSET_BASE = Path.join(__dirname, 'App', 'Assets');
-  const META_ASSET_BASE = Path.join(ASSET_BASE, 'app-icons');
-  const ICON_IMAGE_EXTENSIONS = [
-    'ico',
-    'png',
-    'svg'
-  ];
-  const TEMPLATE_DELIMITERS = {
-    OPEN: '___',
-    CLOSE: '___'
-  };
-  // File paths
-  const ASSETS_PATHS = Glob.sync(Path.join(ASSET_BASE, '*.*'));
-  // Exec options
-  const mergedOptions = getMergedOptions('app', Program);
-  const packageInfo = await new Promise(async (res, rej) => {
-    let pi;// = getPackage();
 
-    if (!pi) {
-      await execCommandInline('npm init');
-
-      pi = getPackage() || {};
-    }
-
-    res(pi);
-  });
-  const {
-    description: packageDescription
-  } = packageInfo || {};
-  const {
-    args: [
-      appName = 'App',
-      appPath = './index.html'
-    ] = [],
-    icons,
-    base
-  } = mergedOptions;
-  const description = packageDescription || 'A JSX application.';
-
-  if (!!icons) {
-    const META_ASSET_PATHS = Glob.sync(Path.join(META_ASSET_BASE, '*.*'));
-  }
 };
 
 exec()
