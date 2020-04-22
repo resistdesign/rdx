ad3655faeefa203c2317739adb6d107be16e1b73
Ryan Graff <ryantgraff@resistdesign.com>
2020-04-21 22:30:32 -0400

    feat: (rdx-app) Add option collection and command execution.

--- src/rdx-app.jsx
+++ src/rdx-app.jsx
@@ -1,27 +1,44 @@
 #!/usr/bin/env node
 
 const Program = require('commander');
+const App = require('./App');
 
 /**
  * The App program.
  * @type {Object}
  * */
 Program
-  .option('-i, --icons', 'Include app icons and metadata.', true)
+  .option('-t, --title', 'The application title. Example: My App', 'App')
+  .option('-p, --description', 'The application description.', '')
+  .option('-r, --theme-color', 'The theme color.', '#ffffff')
   .option('-b, --base <directory>', 'The base directory for app files.', 'src')
+  .option('-i, --icons', 'Include app icons and metadata.', true)
+  .option('-d, --default', 'Is the application the default application?', false)
+  .option('-o, --overwrite', 'Overwrite existing files.', false)
   .parse(process.argv);
 
-// currentWorkingDirectory: string;
-// title: string;
-// description: string;
-// themeColor: string;
-// baseDirectory: string;
-// includeIcons: boolean;
-// isDefaultApp: boolean;
-// overwrite: boolean;
-
+const {
+  title,
+  description,
+  ['theme-color']: themeColor,
+  base: baseDirectory,
+  icons: includeIcons,
+  default: isDefaultApp,
+  overwrite
+} = Program;
 const exec = async () => {
+  const app = new App({
+    currentWorkingDirectory: process.cwd(),
+    title,
+    description,
+    themeColor,
+    baseDirectory,
+    includeIcons,
+    isDefaultApp,
+    overwrite
+  });
 
+  await app.execute();
 };
 
 exec()
