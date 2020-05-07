de62243c4ac096ec7504a69ec82ef3bbd28fcdc5
Ryan Graff <ryantgraff@resistdesign.com>
2020-05-06 22:36:35 -0400

    feat: (rdx-app) Add the basis for input form fields.

--- src/rdx-app.jsx
+++ src/rdx-app.jsx
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Command } from 'commander';
-import { render, Box } from 'ink';
+import { render, Box, Static, Text } from 'ink';
+import BigText from 'ink-big-text';
 import TextInput from 'ink-text-input';
 // const App = require('');
 
@@ -31,51 +32,178 @@ const {
   default: isDefaultApp,
   overwrite
 } = Program;
+const DEFAULT_APP_FORM_PROPS = {
+  title,
+  description,
+  themeColor,
+  baseDirectory,
+  includeIcons,
+  isDefaultApp,
+  overwrite
+};
 
-class AppForm extends Component {
-  state = {
-    appTitle: ''
+type AppFormInput = {
+  title: string,
+  description: string,
+  themeColor: string,
+  baseDirectory: string,
+  includeIcons: boolean,
+  isDefaultApp: boolean,
+  overwrite: boolean
+};
+type AppFormProps = {
+  input: AppFormInput
+};
+type AppFormState = {
+  input: AppFormInput,
+  currentField: string
+};
+
+class AppForm extends Component<AppFormProps, AppFormState> {
+  static getDerivedStateFromProps = (props = {}, state = {}) => {
+    const {
+      input: defaultInput = {}
+    } = props;
+    const {
+      input = {}
+    } = state;
+
+    return {
+      input: {
+        ...defaultInput,
+        ...input
+      },
+      ...state
+    };
   };
 
-  onAppTitleChange = appTitle => this.setState({ appTitle });
+  state = {
+    currentField: 'title'
+  };
+
+  exec = async () => {
+    const {
+      input = {}
+    } = this.state;
+    const app = new App({
+      currentWorkingDirectory: process.cwd(),
+      ...input
+    });
+
+    await app.execute();
+  };
+
+  propChangeHandlerCache = {};
+
+  getPropsChangeHandler = (propName = '') => {
+    if (!(this.propChangeHandlerCache[propName] instanceof Function)) {
+      this.propChangeHandlerCache[propName] = (newValue = '') =>
+        this.setState({ [propName]: newValue });
+    }
+
+    return this.propChangeHandlerCache[propName];
+  };
+
+  fieldMap = {
+    title: 'App Title',
+    description: 'Description'
+  };
+
+  onToggleCurrentField = () => {
+    const {
+      currentField
+    } = this.state;
+
+    let newCurrentField,
+      found = false;
+
+    for (const k in this.fieldMap) {
+      if (!!found) {
+        newCurrentField = k;
+      }
+
+      if (k === currentField) {
+        found = true;
+      }
+    }
+
+    if (!!newCurrentField) {
+      this.setState({ currentField: newCurrentField });
+    } else {
+      this.exec()
+        .then(() => process.exit(0))
+        .catch(e => console.log('There was an error:', e));
+    }
+  };
+
+  renderField = ({ currentField = '', targetField = '' } = {}) => {
+    const {
+      [targetField]: label = ''
+    } = this.fieldMap;
+    const {
+      input: {
+        [targetField]: defaultValue = ''
+      } = {}
+    } = this.props;
+    const {
+      input: {
+        [targetField]: value = ''
+      } = {}
+    } = this.state;
+
+    return currentField === targetField ?
+      (
+        <Box>
+          <Box marginRight={1}>
+            {label} ({defaultValue}):
+          </Box>
+          <TextInput
+            value={value}
+            onChange={this.getPropsChangeHandler(targetField)}
+          />
+        </Box>
+      ) : (
+        <Box>
+          <Box marginRight={1}>
+            {label} ({defaultValue}):
+          </Box>
+          <Text>
+            {value}
+          </Text>
+        </Box>
+      );
+  };
 
   render () {
     const {
-      appTitle
+      currentField
     } = this.state;
+    const fieldList = Object
+      .keys(this.fieldMap);
 
     return (
-      <Box>
-        <Box marginRight={1}>
-          App Title (RDX App):
-        </Box>
-        <TextInput
-          value={appTitle}
-          onChange={this.onAppTitleChange}
-        />
+      <Box
+        flexDirection='column'
+      >
+        <Static>
+          <BigText
+            text='RDX'
+          />
+        </Static>
+        {fieldList.map(targetField => (
+          <Fragment
+            key={`Field:${targetField}`}
+          >
+            {this.renderField({ currentField, targetField })}
+          </Fragment>
+        ))}
       </Box>
     );
   }
 }
 
-render(<AppForm/>);
-
-// const exec = async () => {
-//   const app = new App({
-//     currentWorkingDirectory: process.cwd(),
-//     title,
-//     description,
-//     themeColor,
-//     baseDirectory,
-//     includeIcons,
-//     isDefaultApp,
-//     overwrite
-//   });
-//
-//   await app.execute();
-// };
-//
-// exec()
-//   .then(() => {
-//     process.exit();
-//   });
+render(
+  <AppForm
+    input={DEFAULT_APP_FORM_PROPS}
+  />
+);
