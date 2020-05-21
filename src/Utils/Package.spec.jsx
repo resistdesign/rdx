5dfb931102d0e1b94d8ea205a42f6b2e40ac2033
Ryan Graff <ryantgraff@resistdesign.com>
2020-05-21 00:05:53 -0400

    test: (Package Utils) Add a basic test.

--- src/Utils/Package.spec.jsx
+++ src/Utils/Package.spec.jsx
@@ -1,10 +1,14 @@
+import expect from 'expect.js';
 import {includeParentLevels} from '../../TestUtils';
+import Package from './Package';
 
 export default includeParentLevels(
   __dirname,
   {
     Package: {
-      'should be a class': () => {}
+      'should be a class': () => {
+        expect(Package).to.be.a(Function);
+      }
     }
   }
 );
